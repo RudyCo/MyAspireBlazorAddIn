2a7c08055f46e617f1d235087c0a9a6e25abf463
Rudy Cortembert <RudyCo@users.noreply.github.com>
2024-04-12 07:55:33 +0200

    Start adding Office features

---
+++ MyApp.WebAddIn/Components/Pages/Counter.razor.cs
@@ -0,0 +1,12 @@
+namespace MyApp.WebAddIn.Components.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
