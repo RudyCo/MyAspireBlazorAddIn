2f4c4507ed037baf5dd46d3313725027c2f62e74
Rudy Cortembert <RudyCo@users.noreply.github.com>
2024-04-14 18:28:54 +0200

    Rename Add-in name in Aspire and do not launch the browser for the Add-in.

--- MyApp.AppHost/Program.cs
+++ MyApp.AppHost/Program.cs
@@ -5,6 +5,7 @@ var apiService = builder.AddProject<Projects.MyApp_ApiService>("apiservice");
 builder.AddProject<Projects.MyApp_Web>("webfrontend")
     .WithReference(apiService);
 
-builder.AddProject<Projects.MyApp_AddIn>("myapp-addin");
+builder.AddProject<Projects.MyApp_AddIn>("exceladdin")
+    .WithReference(apiService);
 
 builder.Build().Run();
