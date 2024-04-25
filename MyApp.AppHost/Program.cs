eec8f54ae396cd638bedcca5b525abc0f4427220
Rudy Cortembert <RudyCo@users.noreply.github.com>
2024-04-25 22:57:21 +0200

    Upgrade to .NET Aspire preview-6

--- MyApp.AppHost/Program.cs
+++ MyApp.AppHost/Program.cs
@@ -5,7 +5,6 @@ var apiService = builder.AddProject<Projects.MyApp_ApiService>("apiservice");
 builder.AddProject<Projects.MyApp_Web>("webfrontend")
     .WithReference(apiService);
 
-builder.AddProject<Projects.MyApp_AddIn>("exceladdin")
-    .WithReference(apiService);
+builder.AddProject<Projects.MyApp_AddIn>("exceladdin");
 
 builder.Build().Run();
